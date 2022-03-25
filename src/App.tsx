bad4b63130454778c756ead8eee50d0b49c7488f
Sean Williams <sean475@gmail.com>
2022-03-25 10:32:40 -0400

    Can now Add counters

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 import "./App.css";
 import { Counter } from "./Counter";
 
 function App(): JSX.Element {
+    const [counters, setCounters] = useState<JSX.Element[]>([
+        <Counter key="initial" initialLabel="Text"></Counter>
+    ]);
+    const [label, setLabel] = useState<string>("");
     return (
         <div className="App">
             <header className="App-header">
@@ -13,7 +18,26 @@ function App(): JSX.Element {
                 automatically reload.
             </p>
             <span>Hello!</span>
-            <Counter initialLabel="Text"></Counter>
+            {counters.map((curr: JSX.Element): JSX.Element => curr)}
+            <Form.Group controlId="formCounterLabel">
+                <Form.Label>Counter Label</Form.Label>
+                <Form.Control
+                    value={label}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setLabel(event.target.value)
+                    }
+                />
+            </Form.Group>
+            <button
+                onClick={() =>
+                    setCounters([
+                        ...counters,
+                        <Counter key={label} initialLabel={label}></Counter>
+                    ])
+                }
+            >
+                Add Button
+            </button>
         </div>
     );
 }
